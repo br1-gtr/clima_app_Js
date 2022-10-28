ceadf5b7090d56efdbfaf5dc70e31b1ae55183ee
Br1-NT <magaro.bruno@gmail.com>
2022-10-28 13:34:09 -0300

    img&maq

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const getGeoLoc = () => { //obtiene geolocalizacion de dispositivo
 const getDataApi = pos => {
     console.log(pos);
     const { latitude , longitude } = pos.coords; //asigna lat y long
-    fetch( `https://api.openweathermap.org/data/2.5/weather?q={city name}lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric` )
+    fetch( `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=es` )
     .then( res => res.json() )
     .then( data => renderData(data) )
     .catch( err => console.log('Consulta API Fallida!',err) );
@@ -17,10 +17,25 @@ const getDataApi = pos => {
 
 const renderData = dataApi => {
     console.log(dataApi);
-    const country = dataApi.sys.country;
-    console.log(country);
+    setIcon(dataApi);
+    setInfoWeather(dataApi);
+}
+
+const setIcon = dataApi => { //establece icono segun id
+    const iconCard = document.querySelector('#icon');
+    const idIcon = dataApi.weather[0].icon
+    iconCard.setAttribute('src', `http://openweathermap.org/img/wn/${idIcon}.png`);
+}
+const setInfoWeather = dataApi => {
+    const temp = document.querySelector('.temp');
+    temp.textContent = Math.round(dataApi.main.temp);
+    const hum = document.querySelector('.humidity');
+    hum.textContent = dataApi.main.humidity;
+    const feelsLike = document.querySelector('.feels_like');
+    feelsLike.textContent = Math.round(dataApi.main.feels_like);
+    const windSpeed = document.querySelector('.wind');
+    windSpeed.textContent = dataApi.wind.speed;
 }
 
 getGeoLoc()
-
 
